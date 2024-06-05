c69830c4e5850c939315c36da6b0ac925f18044a
Prince Levis <tamezelv@gmail.com>
2024-06-05 01:45:28 +0100

    good

---
+++ BackEnd/AgenceWedSite/AgenceWedSite/src/main/java/com/agenceWebSite/AgenceWedSite/Controllers/NotificationController.java
@@ -0,0 +1,2 @@
+package com.agenceWebSite.AgenceWedSite.Controllers;public class NotificationController {
+}
---
+++ BackEnd/AgenceWedSite/AgenceWedSite/src/main/java/com/agenceWebSite/AgenceWedSite/Service/NotificationService.java
@@ -0,0 +1,2 @@
+package com.agenceWebSite.AgenceWedSite.Service;public class NotificationService {
+}

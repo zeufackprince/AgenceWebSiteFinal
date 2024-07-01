06f85ed93370d2bc1d54c688c8af057dbfd18d4b
arms237 <134302560+arms237@users.noreply.github.com>
2024-07-01 18:23:02 +0100

    Mise a jour

--- FrontEnd/InterfaceClient/src/components/Auth/AuthRouter.js
+++ FrontEnd/InterfaceClient/src/components/Auth/AuthRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route,Routes } from 'react-router-dom';
 import Login from './Login';
 import Registration from './Registration';
-
+//
 const AuthRouter = () => {
     return (
         <div>
